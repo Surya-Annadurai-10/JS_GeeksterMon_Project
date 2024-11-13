e5737016347b9ae0c504f8e1875a3160fc9e0844
Surya Annadurai <suryaannadurai1041999@gmail.com>
2024-11-13 23:18:48 +0530

    updated

---
+++ script.js
@@ -0,0 +1,114 @@
+let container = document.querySelector(".main");
+
+
+async function poke(params) {
+    let promise = await fetch (params);
+   let result = await promise.json();
+   return result;
+}
+
+
+
+async function fetchPokemon(){
+    for ( let i = 1 ; i <= 200; i++){
+        let pokemon = await poke(`https://pokeapi.co/api/v2/pokemon/${i}`);
+       console.log(pokemon)
+
+       createCard(pokemon);
+    }
+}
+
+fetchPokemon();
+
+function createCard(pokemon){
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML= `
+     <div class="front">
+            <p class="id">#${pokemon.id}</p>
+            <img src="${pokemon.sprites.front_default}" alt="">
+            <h1 class="name">${pokemon.name}</h1>
+            <p class="type">${pokemon.types[0].type.name}</p>
+           </div>
+           <div class="back">
+            <p class="id">#${pokemon.id}</p>
+            <img src="${pokemon.sprites.back_default}" alt="">
+            <h1 class="name">${pokemon.name}</h1>
+            <br class="">Abilities: </br>${abilities(pokemon.abilities)}</p>
+           </div>
+    `;
+
+    if (pokemon.types[0].type.name == 'grass'){
+        card.style.backgroundColor = "#A0CF59"
+    }
+
+    if (pokemon.types[0].type.name == 'fire'){
+        card.style.backgroundColor = "#FD842F"
+    }
+
+    if (pokemon.types[0].type.name == 'water'){
+        card.style.backgroundColor = "#4E98C7"
+    }
+
+    if (pokemon.types[0].type.name == 'bug'){
+        card.style.backgroundColor = "#79A449"
+    }
+
+    if (pokemon.types[0].type.name == 'normal'){
+        card.style.backgroundColor = "#A9B0B3"
+    }
+
+    if (pokemon.types[0].type.name == 'poison'){
+        card.style.backgroundColor = "#BD86CC"
+    }
+
+    if (pokemon.types[0].type.name == 'electric'){
+        card.style.backgroundColor = "#EFD73F"
+    }
+
+    if (pokemon.types[0].type.name == 'ground'){
+        card.style.backgroundColor = "#F7E049"
+    }
+
+    if (pokemon.types[0].type.name == 'fairy'){
+        card.style.backgroundColor = "#FDBDEA"
+    }
+
+    if (pokemon.types[0].type.name == 'fighting'){
+        card.style.backgroundColor = "#D76F2E"
+    }
+
+    if (pokemon.types[0].type.name == 'psychic'){
+        card.style.backgroundColor = "#F46EBD"
+    }
+
+    if (pokemon.types[0].type.name == 'rock'){
+        card.style.backgroundColor = "#A8922C"
+    }
+
+    if (pokemon.types[0].type.name == 'ghost'){
+        card.style.backgroundColor = "#826AA8"
+    }
+
+    if (pokemon.types[0].type.name == 'ice'){
+        card.style.backgroundColor = "#5AC7E8"
+    }
+
+    if (pokemon.types[0].type.name == 'dragon'){
+        card.style.backgroundColor = "#DCAA2B"
+    }
+
+    container.appendChild(card)
+
+
+}
+function abilities(arr){
+    let str = ''
+    arr.forEach((ele) =>{
+        // let res = ele.ability.name.toCapitalize();
+        str = str + ele.ability.name + ",";
+    })
+return str;
+}
+
+console.log(new Date('3-5-2024'))
